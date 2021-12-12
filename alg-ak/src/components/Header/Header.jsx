4872fc3ece68ca6019d6f078f0d47b42d8b83434
silirama89 <92896189+silirama89@users.noreply.github.com>
2021-12-11 19:55:49 -0500

    Update Header.jsx

--- alg-ak/src/components/Header/Header.jsx
+++ alg-ak/src/components/Header/Header.jsx
@@ -4,10 +4,10 @@ import './Header.css'
 
 function Header() {
   return (
-    <div id='header'>
-      <png src="./ak-logo-2@3x.png" alt="company logo" className="logo"/>
-      <h4>“To be a good cook you have to have a love of the good, a love of hard work, and a love of creating."</h4>
-      <p>Julia Child</p>
+    <div className="header">
+      <png src="./ak-logo-2@3x.png" alt="anna_s kitchen logo" className="logo"/>
+      <h5>“To be a good cook you have to have a love of the good, a love of hard work, and a love of creating."</h4>
+      <h6>Julia Child</h6>
     </div>
   )
 }
