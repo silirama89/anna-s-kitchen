f0d2ba3f937fea3a9e78d5e84e2f67971a6935f4
silirama89 <92896189+silirama89@users.noreply.github.com>
2021-12-13 09:33:17 -0500

    Update Gallery.jsx

--- alg-ak/src/components/Home/Gallery.jsx
+++ alg-ak/src/components/Home/Gallery.jsx
@@ -5,7 +5,7 @@ function Gallery() {
     
     <div className='gallery'>
       <img className='pic1' src="https://res.cloudinary.com/silirama89/image/upload/v1639371290/alg-ak/132433_005_ylkdse.jpg" alt="pic 01" />
-      <img className='pic2' src="https://res.cloudinary.com/silirama89/image/upload/v1639271504/alg-ak/132149_004_tq4vmd.jpg" alt="pic 02" />
+      <img className='pic2' src="https://res.cloudinary.com/silirama89/image/upload/v1639371290/alg-ak/132149_007_pqubg9.jpg" alt="pic 02" />
       <img className='pic3' src="https://res.cloudinary.com/silirama89/image/upload/v1639371290/alg-ak/132149_005_m8libq.jpg" alt="pic 03" />
       <img className='pic4' src="https://res.cloudinary.com/silirama89/image/upload/v1639371290/alg-ak/132433_002_klou1c.jpg" alt="pic 04" />
       <img className='pic5' src="https://res.cloudinary.com/silirama89/image/upload/v1639371289/alg-ak/132113_002_p1c86w.jpg" alt="pic 05" />
